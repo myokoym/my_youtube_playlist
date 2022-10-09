8a95359cc951273f25e60637ba6fcd2e55ab0de3
Masafumi Yokoyama <myokoym@gmail.com>
2022-10-09 23:10:19 +0900

    use react-youtube

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,24 @@ import './App.css';
 import Player from "./components/player"
 
 function App() {
+  const timestamps = []
+  timestamps.push({
+    videoId: "-6CMf_gufLI",
+    start: 454,
+    end: 600,
+  })
+  timestamps.push({
+    videoId: "-6CMf_gufLI",
+    start: 2415,
+    end: 2670,
+  })
+
+
   return (
     <div className="App">
-      <Player videoId="-6CMf_gufLI"></Player>
+      <Player
+        timestamps={timestamps}
+      ></Player>
     </div>
   );
 }
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React from 'react'
+import YouTube, { YouTubeProps } from 'react-youtube'
 
 type Props = {
-  videoId: String
+  timestamps: Array<Timestamp>
 }
 
-const Player: React.FC<Props> = ({ videoId }) => {
+type Timestamp = {
+  videoId: string,
+  start: number,
+  end: number,
+}
+
+const Player: React.FC<Props> = ({ timestamps }) => {
+  let index = 0
+  let current = timestamps[index]
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    event.target.pauseVideo();
+  }
+  const onPlayerEnd: YouTubeProps['onEnd'] = (event) => {
+    if (timestamps.length > index + 1) {
+      index++
+      current = timestamps[index]
+      event.target.loadVideoById({
+        videoId: current.videoId,
+        startSeconds: current.start,
+        endSeconds: current.end,
+      })
+    }
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '360',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      start: current.start,
+      end: current.end,
+    },
+  };
   return (
-    <iframe id="ytplayer" width="640" height="360"
-      src={"https://www.youtube.com/embed/" + videoId + "?autoplay=1"}
-    ></iframe>
+    <YouTube
+      videoId={current.videoId}
+      opts={opts}
+      onReady={onPlayerReady}
+      onEnd={onPlayerEnd}
+    />
   );
 }
 
